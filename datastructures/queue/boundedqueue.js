8d0ae9df4adb2b73d06980704d992ae52ae407e7
Andrew Gerst <gerst20051@gmail.com>
2013-10-28 17:23:36 -0400

    added data structures

---
+++ datastructures/queue/boundedqueue.js
@@ -0,0 +1,13 @@
+function BoundedQueue(){
+	
+
+
+}
+
+var queue = new BoundedQueue();
+
+function testQueue(){
+	var digits = [6, 5, 0, 2, 3, 3, 2, 7, 5, 0];
+	
+}
+
---
+++ gates/logicgates.js
@@ -0,0 +1,5 @@
+var x, y, orxy, andxy, notx;
+orxy = x || y;
+andxy = x && y;
+notx = !x;
+console.log("OR: ", orxy);
