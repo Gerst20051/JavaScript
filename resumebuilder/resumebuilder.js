1b146d466b0a0f83256c15d5a891b8bd24e32738
Andrew Gerst <gerst20051@gmail.com>
2013-11-29 11:14:52 -0500

    finished up resume builder

--- resumebuilder/resumebuilder.js
+++ resumebuilder/resumebuilder.js
@@ -4,6 +4,23 @@
  *********************************************
  */
 
+function bbcode(str){
+	/*
+	var matches = [
+		'[url={URL}]{TEXT}[/url]'
+	],
+	templates = [
+		'<a href="{URL}">{TEXT}</a>'
+	],
+	i;
+
+	for (i = 0; i < matches.length; i++) {
+		str = str.replace(matches[i], templates[i]);
+	}
+	*/
+	return str;
+}
+
 var ResumeBuilder = function(){
 	this.data = {};
 	this.selector = "";
@@ -71,21 +88,19 @@ ResumeBuilder.prototype.createModule = function(){
 };
 
 ResumeBuilder.prototype.addTitleModule = function(){
-	var html = [];
+	var html = [], data = this.data;
 	html.push('<div id="titleModule" class="resumemodule">');
 	html.push('<div id="nametitle">');
-	html.push('<div id="name">Andrew Anthony Gerst</div>');
-	html.push('<div id="title">JavaScript Engineer</div>');
+	html.push('<div id="name">' + data.name + '</div>');
+	html.push('<div id="title">' + data.title + '</div>');
 	html.push('</div>');
 	html.push('<div id="contact">');
-	html.push('<div>Chapel Hill, NC, United States</div>');
-	html.push('<div>9198851642</div>');
-	html.push('<div>gerst20051@gmail.com</div>');
-	html.push('<div>http://about.me/agerst</div>');
+	html.push('<div>' + data.location + '</div>');
+	html.push('<div>' + data.number + '</div>');
+	html.push('<div>' + data.email + '</div>');
+	html.push('<div>' + data.url + '</div>');
 	html.push('</div>');
-	html.push('<div id="statement" class="clear">');
-	html.push('I\'m seeking a Web Development position where I can join and collaboratively work with a team and/or independently to build quality web applications or games. I\'ve been a Web Developer since 2005. I started because I wanted a central database accessible from anywhere on the internet of everything I owned. Mainly I wanted to document my games and have cheats for all of my games on my website I named Homenet. Homenet Spaces (HnS) was my first social network my spin on MySpace. I\'ve always seen ways to improve the way we interact with our friends and family. I really want to use my skills to impact the world in life-changing ways. Another passion I have is listening to music so I\'ve created multiple tools that offer different music listening experiences. Click for my book JavaScript Mastery.');
-	html.push('</div>');
+	html.push('<div id="statement" class="clear">' + bbcode(data.statement) + '</div>');
 	html.push('</div>');
 	this.output.push(html.join(''));
 };
@@ -102,48 +117,55 @@ ResumeBuilder.prototype.addSkillsModule = function(){
 };
 
 ResumeBuilder.prototype.addExperienceModule = function(){
-	var html = [];
+	var html = [], experience = this.data.experience, i;
 	html.push('<div id="experienceModule" class="resumemodule">');
 	html.push('<div class="leftcol"><div>Experience</div></div>');
 	html.push('<div class="rightcol">');
-	html.push('<div>');
-	html.push('<div><b>Computer Programmer</b> – UNC Chapel Hill Computer Science Department<span class="datespan">November 2011 - June 2012</span></div>');
-	html.push('<div>html5, css, youtube­api, javascript, dojo</div>');
-	html.push('<div class="responsibilities">');
-	html.push('Accessible YouTube is an interface to YouTube designed for people who access their computers via either 2 switches for children with disabilities or a mouse. http://gbserver2.cs.unc.edu/playpen/AccessibleYouTube/');
-	html.push('</div>');
-	html.push('</div>');
+	for (i = 0; i < experience.length; i++) {
+		html.push('<div class="experiencecontainer">');
+		html.push('<div><b>' + experience[i].title + '</b> – ' + experience[i].organization + '<span class="datespan">' + experience[i].startDate + ' - ' + experience[i].endDate + '</span></div>');
+		if (experience[i].technologies && experience[i].technologies.length) {
+			html.push('<div>' + experience[i].technologies.join(', ') + '</div>');
+		}
+		html.push('<div class="responsibilities">' + experience[i].responsibilities + '</div>');
+		html.push('</div>');
+	}
 	html.push('</div>');
 	html.push('</div>');
 	this.output.push(html.join(''));
 };
 
 ResumeBuilder.prototype.addEducationModule = function(){
-	var html = [], i;
+	var html = [], education = this.data.education, i;
 	html.push('<div id="educationModule" class="resumemodule">');
 	html.push('<div class="leftcol"><div>Education</div></div>');
 	html.push('<div class="rightcol">');
-	html.push('<div>');
-	html.push('<div><b>Bachelor of Arts (B.A.) Computer Science</b> – University of North Carolina at Chapel Hill<span class="datespan">2011 - 2015</span></div>');
-	html.push('<div class="achievements">');
-	html.push('Won second place in a programming competition hosted by Passport Parking and now my picture is up on the wall in the computer science building. My team and I placed third out of eight teams in Facebook\'s 2012 North Carolina Hackathon.');
-	html.push('</div>');
-	html.push('</div>');
+	for (i = 0; i < education.length; i++) {
+		html.push('<div class="educationcontainer">');
+		html.push('<div><b>' + education[i].degree + '</b> – ' + education[i].university + '<span class="datespan">' + education[i].startYear + ' - ' + education[i].endYear + '</span></div>');
+		if (education[i].technologies && education[i].technologies.length) {
+			html.push('<div>' + education[i].technologies.join(', ') + '</div>');
+		}
+		html.push('<div class="achievements">' + education[i].achievements + '</div>');
+		html.push('</div>');
+	}
 	html.push('</div>');
 	html.push('</div>');
 	this.output.push(html.join(''));
 };
 
 ResumeBuilder.prototype.addProjectsModule = function(){
-	var html = [], i;
+	var html = [], projects = this.data.projects, i;
 	html.push('<div id="projectsModule" class="resumemodule">');
 	html.push('<div class="leftcol"><div>Projects</div></div>');
 	html.push('<div class="rightcol">');
-	for (i = 0; i < this.data.projects.length; i++) {
+	for (i = 0; i < projects.length; i++) {
 		html.push('<div class="projectcontainer">');
-		html.push('<div><b>HnS Wave</b> – https://github.com/Gerst20051/HnS­Wave</div>');
-		html.push('<div>javascript, php</div>');
-		html.push('<div class="desc">New Wave Web Development. HnS Quotes and WebSnapChat.</div>');
+		html.push('<div><b>' + projects[i].name + '</b> – ' + projects[i].url + '</div>');
+		if (projects[i].technologies && projects[i].technologies.length) {
+			html.push('<div>' + projects[i].technologies.join(', ') + '</div>');
+		}
+		html.push('<div class="desc">' + projects[i].desc + '</div>');
 		html.push('</div>');
 	}
 	html.push('</div>');
