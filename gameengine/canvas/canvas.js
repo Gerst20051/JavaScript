83634b4e09698eb3b554b71fc892968b4dce79bd
Andrew Gerst <gerst20051@gmail.com>
2013-12-03 06:29:09 -0500

    updated canvas engine

--- gameengine/canvas/canvas.js
+++ gameengine/canvas/canvas.js
@@ -87,15 +87,20 @@ c = {
 	H: 0,
 	X: 0,
 	Y: 0,
+	O: 0.5,
+	grid: false,
 	background: "rgba(0, 0, 0, 1)",
 	fillStyle: "rgba(0, 0, 0, 1)",
-	fill: true,
+	doFill: true,
 	strokeStyle: "rgba(0, 0, 0, 1)",
 	lineWidth: 1,
-	stroke: true,
+	lineWidthHalf: 0.5,
+	lineCap: "round",
+	doStroke: true,
 	color: [0, 0, 0, 0],
-	font: "Arial",
+	font: "12px Arial",
 	fontSize: 12,
+	fontStyle: "Arial",
 	mouseX: 0,
 	mouseY: 0,
 	mousePressed: false,
@@ -111,82 +116,122 @@ debug = function(){
 size = function(w, h){
 	canvas.size(w, h);
 },
-reload = function(){
-	context.lineWidth = c.lineWidth;
-	context.strokeStyle = c.strokeStyle;
-	//context.stroke();
+engage = function(){
+	ctx.fillStyle = c.fillStyle;
+	ctx.strokeStyle = c.strokeStyle;
+	ctx.lineWidth = c.lineWidth;
+	ctx.lineCap = c.lineCap;
+	ctx.font = c.font;
+	ctx.beginPath();
+},
+paint = function(){
+	ctx.closePath();
+	ctx.stroke();
 },
 /* Shapes */
 point = function(x, y){ // draw a point
-	context.fillRect(x, y, c.lineWidth, c.lineWidth);
+	engage();
+	ctx.fillRect(x + c.O, y + c.O, c.lineWidth, c.lineWidth);
+	paint();
 },
 line = function(x1, y1, x2, y2){ // draw a line
-
+	engage();
+	ctx.moveTo(x1 + c.O + c.lineWidthHalf, y1 + c.O + c.lineWidthHalf);
+	ctx.lineTo(x2 + c.O + c.lineWidthHalf, y2 + c.O + c.lineWidthHalf);
+	paint();
 },
 rect = function(x, y, w, h){ // draw a rectangle
-	/*
-	context.fillStyle = "#009900";
-	context.fillRect(10, 10, 100, 100);
-	context.strokeStyle = "rgba(0, 0, 255, 0)";
-	context.lineWidth = 5;
-	context.strokeRect(10, 10, 100, 100);
-	*/
+	engage();
+	if (c.doStroke && !c.doFill) {
+		ctx.strokeRect(x + c.O, y + c.O, w, h);
+	} else if (c.doFill && !c.doStroke) {
+		ctx.fillRect(x + c.O, y + c.O, w, h);
+	} else if (c.doStroke && c.doFill) {
+		if (c.fillStyle === c.strokeStyle) {
+			ctx.fillRect(x + c.O, y + c.O, w, h);
+		} else {
+			ctx.fillRect(x + c.O, y + c.O, w, h);
+			ctx.strokeRect(x + c.O, y + c.O, w, h);
+		}
+	}
+	paint();
 },
 arc = function(x, y, w, h, start, stop){ // draw an arc
-
+	engage();
 },
 circle = function(x, y, radius){ // draw a circle
-	context.beginPath();
-	context.arc(x, y, radius, 0, constants.PI * 2, false);
-	context.closePath();
+	engage();
+	ctx.arc(x + c.O, y + c.O, radius, 0, constants.PI * 2, false);
+	paint();
 },
 ellipse = function(x, y, w, h){ // draw an ellipse
-
+	engage();
 },
 triangle = function(x1, y1, x2, y2, x3, y3){ // draw a triangle
-
+	engage();
 },
 bezier = function(x1, y1, cx1, cy1, cx2, cy2, x2, y2){ // draw a bezier curve
-
+	engage();
 },
 quad = function(x1, y1, x2, y2, x3, y3, x4, y4){ // draw any quadrilateral
-
+	engage();
 },
 image = function(image, x, y){ // display an image
-
+	engage();
 },
 /* Coloring */
 background = function(r, g, b, a){ // set the background color
-	// set fillStyle
-	// fillRect
+	var oldFillStyle = c.fillStyle;
+	c.background = "rgba(" + r + ", " + g + ", " + b + ", 1)";
+	c.fillStyle = c.background;
+	engage();
+	ctx.fillRect(0, 0, c.W, c.H);
+	paint();
+	c.fillStyle = oldFillStyle;
 },
 fill = function(r, g, b, a){ // fill color for shapes / text color
-
+	c.doFill = true;
+	c.fillStyle = "rgba(" + r + ", " + g + ", " + b + ", 1)";
 },
 noFill = function(){ // no fill for shapes
-
+	c.doFill = false;
 },
 stroke = function(r, g, b, a){ // outline color for shapes / text color
-
+	c.doStroke = true;
+	c.strokeStyle = "rgba(" + r + ", " + g + ", " + b + ", 1)";
 },
 strokeWeight = function(thickness){ // no outline for shapes
+	c.doStroke = true;
 	c.lineWidth = thickness;
+	c.lineWidthHalf = thickness / 2;
 },
 noStroke = function(){ // no outline for shapes
-	c.stroke = false;
+	c.doStroke = false;
 },
 color = function(r, g, b, a){ // store a color in a variable
 
 },
 /* Text */
 text = function(text, x, y){ // draw some text
-
+	var oldStrokeWeight = c.strokeWeight;
+	c.strokeWeight = 1;
+	engage();
+	if (c.doStroke && !c.doFill) {
+		ctx.strokeText(text, x, y);
+	} else if (c.doFill) {
+		ctx.fillText(text, x, y);
+	}
+	stroke();
+	c.strokeWeight = oldStrokeWeight;
 },
 textFont = function(font, size){ // changes the font of the text
-
+	c.fontStyle = font;
+	c.fontSize = size;
+	c.font = size + "px " + font;
 },
 textSize = function(size){ // change the size of text
-
+	c.textSize = size;
+	c.font = size + "px " + c.fontStyle;
 },
 /* Math */
 random = function(low, high){ // generate a random number
@@ -214,18 +259,20 @@ tan = function(deg){ // take the tangent of an angle
 
 },
 grid = function(interval){
+	engage();
 	if (!interval) {
 		interval = 10;
 	}
-	for (var x = 0.5; x <= c.W; x += interval) { // vertical
-		context.moveTo(x, 0);
-		context.lineTo(x, c.H);
+	for (var x = c.O; x <= c.W; x += interval) { // vertical
+		ctx.moveTo(x, 0);
+		ctx.lineTo(x, c.H);
 	}
-	for (var y = 0.5; y <= c.H; y += interval) { // horizontal
-		context.moveTo(0, y);
-		context.lineTo(c.W, y);
+	for (var y = c.O; y <= c.H; y += interval) { // horizontal
+		ctx.moveTo(0, y);
+		ctx.lineTo(c.W, y);
 	}
-	reload();
+	c.grid = true;
+	paint();
 };
 
 /*
@@ -251,7 +298,6 @@ function Canvas(canvas){
 	this.animloop = function(){
 		window.requestAnimFrame(this.animloop);
 		this.draw();
-		context.stroke();
 	}.bind(this);
 
 	this.size = function(w, h){
