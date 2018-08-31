dcc33c74d45f602803c90165024c889f0cfa79b6
Andrew Gerst <gerst20051@gmail.com>
2018-08-31 00:21:30 -0400

    show number of itm vs. otm spreads available

--- option-spreads/script.js
+++ option-spreads/script.js
@@ -1,5 +1,13 @@
-const host = document.location.origin;
-var availableDates = [], optionSpreads = [], optionSpreadIndex = null, expirations = [], selectedExpiration = '', spreadType = [], spreadListIndex = null;
+const isLocal = false;
+const host = isLocal ? 'http://127.0.0.1' : document.location.origin;
+
+var availableDates = [],
+    optionSpreads = [],
+    optionSpreadIndex = null,
+    expirations = [],
+    selectedExpiration = '',
+    spreadType = [],
+    spreadListIndex = null;
 
 $(document).ready(() => {
     $('#loadingDataHeaderContent, #loadingDataContent').show();
@@ -106,6 +114,7 @@ function loadOptionSpreads(date) {
         $('#availableDatesListContent').hide();
         $('#instrumentsListContent').show();
         $('#loadingDataHeaderContent, #loadingDataContent').hide();
+        window.scrollTo(0, 0);
         $('#loadedDataHeaderContent, #loadedDataContent').show();
     });
 }
@@ -128,12 +137,52 @@ $('#instrumentsList').on('click', '.fullRow', function () {
     });
     $('#expirationList').html(rowItems);
     $('#instrumentsListContent').hide();
+    window.scrollTo(0, 0);
     $('#expirationListContent').show();
 });
 
 $('#expirationList').on('click', '.fullRow', function () {
     selectedExpiration = $(this).find('span').text();
+    const callCreditSpreads = optionSpreads[optionSpreadIndex].spreads[selectedExpiration]['call']['credit'];
+    const callCreditSpreadsItm = callCreditSpreads.filter(spread => spread.itm);
+    const callDebitSpreads = optionSpreads[optionSpreadIndex].spreads[selectedExpiration]['call']['debit'];
+    const callDebitSpreadsItm = callDebitSpreads.filter(spread => spread.itm);
+    const putCreditSpreads = optionSpreads[optionSpreadIndex].spreads[selectedExpiration]['put']['credit'];
+    const putCreditSpreadsItm = putCreditSpreads.filter(spread => spread.itm);
+    const putDebitSpreads = optionSpreads[optionSpreadIndex].spreads[selectedExpiration]['put']['debit'];
+    const putDebitSpreadsItm = putDebitSpreads.filter(spread => spread.itm);
+    const callCreditSpreadsDescription = callCreditSpreads.length ? `${callCreditSpreadsItm.length} ITM / ${callCreditSpreads.length - callCreditSpreadsItm.length} OTM` : 'None Available';
+    const callDebitSpreadsDescription = callDebitSpreads.length ? `${callDebitSpreadsItm.length} ITM / ${callDebitSpreads.length - callDebitSpreadsItm.length} OTM` : 'None Available';
+    const putCreditSpreadsDescription = putCreditSpreads.length ? `${putCreditSpreadsItm.length} ITM / ${putCreditSpreads.length - putCreditSpreadsItm.length} OTM` : 'None Available';
+    const putDebitSpreadsDescription = putDebitSpreads.length ? `${putDebitSpreadsItm.length} ITM / ${putDebitSpreads.length - putDebitSpreadsItm.length} OTM` : 'None Available';
+    $('#spreadTypeList').html(`
+        <li class="clearfix">
+            <div class="fullRow">
+                <span><b>Call Credit Spreads</b></span><br>
+                <span>${callCreditSpreadsDescription}</span>
+            </div>
+        </li>
+        <li class="clearfix">
+            <div class="fullRow">
+                <span><b>Call Debit Spreads</b></span><br>
+                <span>${callDebitSpreadsDescription}</span>
+            </div>
+        </li>
+        <li class="clearfix">
+            <div class="fullRow">
+                <span><b>Put Credit Spreads</b></span><br>
+                <span>${putCreditSpreadsDescription}</span>
+            </div>
+        </li>
+        <li class="clearfix">
+            <div class="fullRow">
+                <span><b>Put Debit Spreads</b></span><br>
+                <span>${putDebitSpreadsDescription}</span>
+            </div>
+        </li>
+    `);
     $('#expirationListContent').hide();
+    window.scrollTo(0, 0);
     $('#spreadTypeListContent').show();
 });
 
@@ -157,6 +206,7 @@ $('#spreadTypeList').on('click', '.fullRow', function () {
     });
     $('#spreadList').html(rowItems);
     $('#spreadTypeListContent').hide();
+    window.scrollTo(0, 0);
     $('#spreadListContent').show();
 });
 
